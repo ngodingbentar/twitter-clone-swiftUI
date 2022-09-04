310ec7ed40f2efc3405acebaf5988627d242d27f
ngodingbentar <dewaqintoro@gmail.com>
2022-09-04 18:39:39 +0700

    added: texteditor

--- twitterClone/Core/Feed/View/FeedView.swift
+++ twitterClone/Core/Feed/View/FeedView.swift
@@ -1,13 +1,41 @@
+
+
+
+
+
+
+
 import SwiftUI
 
 struct FeedView: View {
+    @State private var showNewTweetView = false
+    
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(0 ... 20, id: \.self) { _ in
-                    TweetRowViews()
-                        .padding()
+        ZStack(alignment: .bottomTrailing) {
+            ScrollView {
+                LazyVStack {
+                    ForEach(0 ... 20, id: \.self) { _ in
+                        TweetRowViews()
+                            .padding()
+                    }
                 }
+            }
+            
+            Button {
+                showNewTweetView.toggle()
+            } label: {
+                Image(systemName: "pencil")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 28, height: 28)
+                    .padding()
+            }
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .padding()
+            .fullScreenCover(isPresented: $showNewTweetView) {
+                NewTweetView()
             }
         }
     }
---
+++ twitterClone/Core/UploadTweet/Views/NewTweetView.swift
@@ -0,0 +1,56 @@
+//
+//  NewTweetView.swift
+//  twitterClone
+//
+//  Created by Dewa Qintoro on 04/09/22.
+//
+
+import SwiftUI
+
+struct NewTweetView: View {
+    @State private var caption = ""
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text("Cancel")
+                        .foregroundColor(Color(.systemBlue))
+                }
+                
+                Spacer()
+                
+                Button {
+                    print("Tweet")
+                } label: {
+                    Text("Tweet")
+                        .bold()
+                        .padding(.horizontal)
+                        .padding(.vertical, 8)
+                        .background(Color(.systemBlue))
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                }
+                
+            }
+            .padding()
+            
+            HStack(alignment: .top) {
+                Circle()
+                    .frame(width: 64, height: 64)
+                
+                TextArea("coba aja", text: $caption)
+            }
+            .padding()
+        }
+    }
+}
+
+struct NewTweetView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTweetView()
+    }
+}
