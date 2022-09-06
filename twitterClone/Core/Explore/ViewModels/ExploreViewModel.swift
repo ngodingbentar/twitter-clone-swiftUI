c11a1877cdf791e0ec9e287a85fb6b6c0a5f1b6b
ngodingbentar <dewaqintoro@gmail.com>
2022-09-06 22:09:10 +0700

    updated: fetch data from firebase & search user in expore page

---
+++ twitterClone/Core/Explore/ViewModels/ExploreViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  ExploreViewModel.swift
+//  twitterClone
+//
+//  Created by Dewa Qintoro on 06/09/22.
+//
+
+import Foundation
+
+class ExploreViewModel: ObservableObject {
+    @Published var users = [User]()
+    @Published var searchText = ""
+    
+    var searchhableUsers: [User] {
+        if searchText.isEmpty {
+            return users
+        } else {
+            let lowercasedQuery = searchText.lowercased()
+            
+            return users.filter({
+                $0.username.contains(lowercasedQuery) ||
+                $0.fullname.lowercased().contains(lowercasedQuery)
+            })
+        }
+    }
+    
+    let service = UserService()
+    
+    init() {
+        fetchUsers()
+    }
+    
+    func fetchUsers() {
+        service.fetchUsers { users in
+            self.users = users
+            
+            print("debug: users \(users)")
+        }
+    }
+}
